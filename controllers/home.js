a73528ce9cc893f3698f8fca697768197cb17be8
AngelosGian <angelos.giannourakos@gmail.com>
2022-10-21 07:46:53 +0300

    adding create controller

--- controllers/home.js
+++ controllers/home.js
@@ -2,8 +2,15 @@ const Manga = require('../models/Manga')
 module.exports = {
     getIndex: (req,res)=>{
         res.render('index.ejs')
-    }
+    },
     createManga: (req,res)=>{
+        Manga.create({
+            'title': req.body.mangatitle,
+            'volumes': req.body.mangavolumes,
+            'completed': req.body.mangacompleted
         
+        })
+        
+        res.redirect('/')
     }
 }
