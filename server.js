38f81d899c0aed0e5f4eec200ad96f0402aa14d8
AngelosGian <angelos.giannourakos@gmail.com>
2022-11-09 09:01:13 +0200

    minor changes

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
-const  express = require('express')
-const  app = express()
-const  dotenv = require('dotenv')
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 const connectDB = require('./config/db')
 const mainRoutes = require('./routes/home')
 
 dotenv.config({path: './config/.env'});
 
+// Connect to DB
 connectDB()
 
-
+//General Setting 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+// Routing
 app.use('/', mainRoutes)
 
 
